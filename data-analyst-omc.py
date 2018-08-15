a7b427082a4a1927755b7b6cac9c81c68b97e344
Ahmed Salam <maherslam64@gmail.com>
2018-08-15 16:16:25 +0200

    Mean Method

--- data-analyst-omc.py
+++ data-analyst-omc.py
@@ -1,1 +1,22 @@
 class DataAnalyst:
+
+    data = list()
+
+    def __init__(self):
+        pass
+
+    def mean(self):
+        """We calculate the mean by adding all of our values together, and dividing by the number of values in our dataset"""
+        string = ""
+        for num in self.data:
+            string = string + str(num) + "+"
+        string = string[:-1]
+        print("1:-adding all of our values together:\n{0}".format(string))
+
+        total = sum(self.data)
+        print("total", total)
+        print("2.and dividing by the number of values ({0}) in our dataset ".format(len(self.data)))
+        print(total, "/", len(self.data), "is", total / len(self.data))
+
+        return total / len(self.data)
+
